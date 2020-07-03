6ac4cbc4465ed23ee9f55647c86cc7f511d1fe4a
dark00shadow <twistcharlievlogs@gmail.com>
2020-07-03 20:55:03 +0100

    Update Main.py

--- scr/Main.py
+++ scr/Main.py
@@ -1,6 +1,19 @@
 import pyglet
 from pyglet.window import mouse, key
+from RectangleCollision import *
 
-StartMenu = False
+StartMenu = True
+
+# Start menu
+
 class Start():
     Button_Image = pyglet.image.load('StartButton.png')
+
+
+# Window
+
+WINDOW = pyglet.window.Window(caption='noodle-game', width=600, height=600)
+KeyHandler = key.KeyStateHandler()
+MouseHandler = mouse.MouseStateHandler()
+WINDOW.push_handlers(KeyHandler)
+WINDOW.push_handlers(MouseHandler)
